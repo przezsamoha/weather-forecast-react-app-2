3a172fb3c55bfeca0b790e892714010fed855168
Magdalena Horwat <przezsamoha@gmail.com>
2023-02-23 13:22:47 +0100

    vite config changes - base removed

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weather-forecast-react-app/",
   plugins: [react(), eslint()],
   test: {
     globals: true,
