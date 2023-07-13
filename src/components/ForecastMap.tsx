7e534ef52f1d1e994fedfdbecf616c50d7aac9b7
Magdalena Horwat <przezsamoha@gmail.com>
2023-07-13 20:10:22 +0200

    react-leaflet and leaflet dependencies added, ForecastMap component updated

--- src/components/ForecastMap.tsx
+++ src/components/ForecastMap.tsx
@@ -1,3 +1,18 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+// import { CityProps } from "../types";
+
+// interface ForecastMapProps {
+//   lon: CityProps;
+//   lat: CityProps;
+// }
+
 export default function ForecastMap() {
-  return <div id="map" className="h-44"></div>;
+  return (
+    <MapContainer className="h-44" center={[52.2319581, 21.0067249]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
 }
